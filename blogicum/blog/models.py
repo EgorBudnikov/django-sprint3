b4351abb6c55eeec3f596f6ca079c507572cbbc8
Egor Budnikov <budnikov16.08@ya.ru>
2025-04-22 16:57:22 +0800

    5 попытка

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,8 +6,10 @@ User = get_user_model()
 
 class PublishedModel(models.Model):
     is_published = models.BooleanField(
-        'Опубликовано', default=True,
-        help_text='Снимите галочку, чтобы скрыть публикацию.')
+        'Опубликовано',
+        default=True,
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
     created_at = models.DateTimeField('Добавлено', auto_now_add=True)
 
     class Meta:
@@ -18,11 +20,13 @@ class Category(PublishedModel):
     title = models.CharField('Заголовок', max_length=256)
     description = models.TextField('Описание')
     slug = models.SlugField(
-        'Идентификатор', unique=True,
+        'Идентификатор',
+        unique=True,
         help_text=(
             'Идентификатор страницы для URL; разрешены символы латиницы, '
-            'цифры, дефис и подчёркивание.')
+            'цифры, дефис и подчёркивание.'
         )
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -49,8 +53,9 @@ class Post(PublishedModel):
     pub_date = models.DateTimeField(
         'Дата и время публикации',
         help_text=(
-                'Если установить дату и время в будущем '
-                '— можно делать отложенные публикации.')
+            'Если установить дату и время в будущем — можно делать '
+            'отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
         User,
@@ -74,6 +79,7 @@ class Post(PublishedModel):
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        ordering = ['-pub_date']
 
     def __str__(self):
         return self.title
